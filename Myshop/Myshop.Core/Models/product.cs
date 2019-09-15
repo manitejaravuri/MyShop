0a793b0aec9e51795802c994a35561814ebb61cd
manitejaravuri <54357347+manitejaravuri@users.noreply.github.com>
2019-09-15 10:26:06 +0530

    productmodel and inmemory productRepositary built

---
+++ Myshop/Myshop.Core/Models/product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myshop.Core.Models
+{
+   public class product
+    {
+        public string id { get; set; }
+        [StringLength(20)]
+        [DisplayName("product name")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        [Range(0,10000)]
+        public decimal price { get; set; }
+
+        public string categeory { get; set; }
+
+        public string image { get; set; }
+
+
+        public product()
+        {
+            this.id = Guid.NewGuid().ToString();
+        }
+    }
+}
